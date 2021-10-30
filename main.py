36336795e1a1ff5d312961ffcad5dafa886615ae
Lu Zhihao <lu_zhihao@u.nus.edu>
2021-10-30 19:03:55 +0800

    working 2nd layer conversation

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 # Import necessary libraries:
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton  
- #upm package(python-telegram-bot)
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler, ConversationHandler
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler, \
+    ConversationHandler
 from db import Database
-
-
+from functools import partial
 db = Database()
 db.create_tables()
 
-def start(update: Update, context: CallbackContext) -> None:
+
+def start(update: Update, context):
     """Sends a message with three inline buttons attached."""
     keyboard = [
         [InlineKeyboardButton("Track Exercise", callback_data='1')],
@@ -22,10 +22,9 @@ def start(update: Update, context: CallbackContext) -> None:
     return "from_start"
 
 
-    
-
-def help_command(update: Update, context: CallbackContext) -> None:
+def help_command(update: Update, context):
     update.message.reply_text("shape is: ")
+
 
 '''
 def button(update: Update, context: CallbackContext) -> None:
@@ -40,38 +39,55 @@ def button(update: Update, context: CallbackContext) -> None:
 
     query.edit_message_text(text=a)
 '''
+def ask_exercise(update, context, exercise=""):
+
+def log_exercise(update, context, exercise=""):
+    query = update.callback_query
+    user = query.from_user.username
+    query.answer()
+
+    if exercise == "PU":
+
+        # db.insert_entry(user, exercise, 3, 2)
+        print(user)
+    elif exercise == "C":
+        db.insert_entry(user, exercise, 3, 2)
+        print(user)
+    elif exercise == "R":
+        db.insert_entry(user, exercise, 3, 2)
+        print(user)
+    query.edit_message_text("Please enter how many: ")
 
 
-def log(update: Update, context: CallbackContext) -> None:
-  db.insert_entry(update.message.from_user.username, update.message.text, 3, 2)
 
-def get_one(update: Update, context: CallbackContext) -> None:
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(db.get_all())
+def get_one(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(db.get_all())
 
-def choose_exercise(update: Update, context: CallbackContext):
-  query = update.callback_query
-  query.answer()
 
-  keyboard = [
-    [InlineKeyboardButton("Pull ups", callback_data='PU'),
-    InlineKeyboardButton("Core", callback_data='C'),
-    InlineKeyboardButton("Run", callback_data='R')],
-    [InlineKeyboardButton("Back", callback_data='back')]
-  ]
-  reply_markup = InlineKeyboardMarkup(keyboard)
-  query.edit_message_text(
-    text="Choose exercise: ", reply_markup=reply_markup
-  )
+def choose_exercise(update, context):
+    query = update.callback_query
+    query.answer()
 
-  return "selected_exercise"
+    keyboard = [
+        [InlineKeyboardButton("Pull ups", callback_data='PU'),
+         InlineKeyboardButton("Core", callback_data='C'),
+         InlineKeyboardButton("Run", callback_data='R')],
+        [InlineKeyboardButton("Back", callback_data='back')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Choose exercise: ", reply_markup=reply_markup
+    )
+
+    return "selected_exercise"
+
 
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token.
     updater = Updater(os.environ['TOKEN'])
-
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -82,26 +98,27 @@ def main() -> None:
     # ^ means "start of line/string"
     # $ means "end of line/string"
     # So ^ABC$ will only allow 'ABC'
-    dispatcher.add_handler(ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            "from_start": [
-                CallbackQueryHandler(choose_exercise, pattern='1'),
-                CallbackQueryHandler(get_one, pattern='2')
-            ],
-            "selected_exercise": [
-                MessageHandler(choose_exercise, pattern='1'),
-                MessageHandler(get_one, pattern='2')
-            ]
-        },
-        fallbacks=[CommandHandler('start', start)],
-      per_message=True
-    )
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler('start', start)],
+            states={
+                "from_start": [
+                    CallbackQueryHandler(choose_exercise, pattern='1'),
+                    CallbackQueryHandler(get_one, pattern='2')
+                ],
+                "selected_exercise": [
+                    CallbackQueryHandler(partial(log_exercise, exercise="PU"), pattern='PU'),
+                    CallbackQueryHandler(partial(log_exercise, exercise="C"), pattern='C'),
+                    CallbackQueryHandler(partial(log_exercise, exercise="R"), pattern='R'),
+                ]
+            },
+            fallbacks=[CommandHandler('start', start)],
+            per_message=False
+        )
     )
 
     # Add ConversationHandler to dispatcher that will be used for handling updates
-    conv_handler)
-    dispatcher.add_handler(CommandHandler("add_entry", log))
+    dispatcher.add_handler(CommandHandler("add_entry", log_exercise))
     # updater.dispatcher.add_handler(CallbackQueryHandler(button))
     dispatcher.add_handler(CommandHandler("get_one", get_one))
     # Start the Bot
@@ -111,6 +128,7 @@ def main() -> None:
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
