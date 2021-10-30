442b5b68879a63b7e54a00f26bcba371fbaba1ab
dumes <65158344+zhihao-lu@users.noreply.github.com>
2021-10-30 09:01:08 +0000

    Not working conversation handler

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
 import os
 # Import necessary libraries:
-from telegram import Update #upm package(python-telegram-bot)
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, InlineKeyboardMarkup, InlineKeyboardButton  
-
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton  
+ #upm package(python-telegram-bot)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler, ConversationHandler
 from db import Database
 
-'''
-d = {"auth_provider_x509_cert_url" : os.environ["auth_provider_x509_cert_url"],
-"auth_uri" : os.environ["auth_uri"],
-"client_email": os.environ["client_email"],
-"client_id": os.environ["client_id"],
-"client_x509_cert_url": os.environ["client_x509_cert_url"],
-"private_key": os.environ["PRIVATE_KEY"].replace('\\n', '\n'),
-"private_key_id": os.environ["private_key_id"],
-"project_id": os.environ["project_id"],
-"token_uri": os.environ["token_uri"],
-"type": os.environ["type"]}
 
-# Set your path:
-
-# Set scope to use when authenticating:
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/spreadsheets',
-         'https://www.googleapis.com/auth/drive.file',
-         'https://www.googleapis.com/auth/drive']
-# Authenticate using your credentials, saved in JSON in Step 1:
-creds = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict=d, scopes=scope)
-
-# Initialize the client, and open the sheet by name:
-client = gspread.authorize(creds)
-sheet = client.open('suite').sheet1
-# Get data from the sheet:
-data = gspread_dataframe.get_as_dataframe(sheet)
-df = data.drop(columns=[c for c in data.columns if "Unnamed" in c]).dropna(how="all")
-'''
 db = Database()
 db.create_tables()
 
@@ -48,26 +20,72 @@ def start(update: Update, context: CallbackContext) -> None:
 
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
+
+
+    
+
 def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("shape is: ")
+
+'''
+def button(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+    if query.data == "2":
+      a = db.get_all()
+
+    query.edit_message_text(text=a)
+'''
+
 
 def log(update: Update, context: CallbackContext) -> None:
   db.insert_entry(update.message.from_user.username, update.message.text, 3, 2)
 
 def get_one(update: Update, context: CallbackContext) -> None:
+  query = update.callback_query
+  query.answer()
   update.message.reply_text(db.get_all())
 
+def track_exercise(update: Update, context: CallbackContext):
+  query = update.callback_query
+  query.answer()
+
+  keyboard = [
+    [InlineKeyboardButton("Pull ups", callback_data='PU'),
+    InlineKeyboardButton("Core", callback_data='C'),
+    InlineKeyboardButton("Run", callback_data='R')]
+  ]
+  reply_markup = InlineKeyboardMarkup(keyboard)
+  query.edit_message_text(
+    text="Choose exercise: ", reply_markup=reply_markup
+  )
 
 def main():
 
     updater = Updater(os.getenv("TOKEN"))
-
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", help_command))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    conv_handler = ConversationHandler(
+      entry_points=[CommandHandler('start', start)],
+      states={
+        "1": [CallbackQueryHandler(track_exercise)],
+        "2": [CallbackQueryHandler(get_one)]
+          },
+      fallbacks=[CommandHandler('start', start)],
+    )
+
+
+    # dispatcher.add_handler(CommandHandler("start", start))
+    # dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("add_entry", log))
+    # updater.dispatcher.add_handler(CallbackQueryHandler(button))
     dispatcher.add_handler(CommandHandler("get_one", get_one))
     # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, log))
+    dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
 
