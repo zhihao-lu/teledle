5099faca6bf613412022e6be4ffcebbb2ad35722
zhihao-lu <65158344+zhihao-lu@users.noreply.github.com>
2022-01-19 11:39:41 +0800

    Create losing path

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ WORD = "EMILY"
 
 CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
 
-reply_keyboard = [[InlineKeyboardButton("Yes", callback_data='Yes')]]
+reply_keyboard = [[InlineKeyboardButton("Yes", callback_data='start_game')]]
 markup = InlineKeyboardMarkup(reply_keyboard)
 
 
@@ -46,14 +46,25 @@ def start(update: Update, context: CallbackContext) -> int:
     return "start"
 
 
+def show_back_home(update, context):
+    query = update.callback_query
+    query.answer()
+
+    text = "Welcome back. Play zhihao-dle"
+    query.edit_message_text(text, reply_markup=markup)
+    return ""
+
+
 def start_game(update, context):
     query = update.callback_query
     query.answer()
+    context.user_data["num_guesses"] = 0
     context.user_data["guess_string"] = "Guess a 5 letter word\. Correct characters are in *bold*, correct characters "\
                                         "in the wrong position are in _italics_\. Wrong characters appear at the side\." \
-                                        " \n \_ \_ \_ \_ \_ \n "
+                                        " \n \_ \_ \_ \_ \_ \n"
     context.user_data["remaining_chars"] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    # Send the key to the user
+
+
     query.edit_message_text(context.user_data["guess_string"], parse_mode=ParseMode.MARKDOWN_V2)
     return "guess"
 
@@ -81,10 +92,18 @@ def generate_guess_string(answer, guess, context):
     return out, answer == guess
 
 
+def valid_guess(guess):
+    return len(guess) == 5
+
+
 def verify_guess(update, context):
-    name = update.message.from_user.first_name
-    tele = update.message.from_user.username
     guess = update.message.text
+
+    if not valid_guess(guess):
+        update.message.reply_text("5 letters only!")
+        return "guess"
+
+    context.user_data["num_guesses"] = context.user_data["num_guesses"] + 1
     guess_string, has_won = generate_guess_string(WORD, guess, context)
 
     context.user_data["guess_string"] = context.user_data["guess_string"] + guess_string
@@ -97,8 +116,21 @@ def verify_guess(update, context):
         # reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_text(context.user_data["guess_string"])  # , reply_markup=reply_markup)
         return "win"
-    update.message.reply_text(context.user_data["guess_string"] + "\n\n" + \
-                              "Characters remaining: \n" + \
+    elif context.user_data["num_guesses"] > 5:
+        keyboard = [
+            [InlineKeyboardButton("Play again", callback_data='start_game')],
+            [InlineKeyboardButton("Back", callback_data='return_menu')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        update.message.reply_text(context.user_data["guess_string"] + "\n\n" +
+                                  f"\n Ran out of guesses\! The word was *{WORD}*\.",
+                                  parse_mode=ParseMode.MARKDOWN_V2,
+                                  reply_markup=reply_markup)
+        return "lose"
+    update.message.reply_text(context.user_data["guess_string"] + "\n\n" +
+                              f"Guesses remaining: *{6 - context.user_data['num_guesses']}*\.\n"
+                              "Characters remaining: \n" +
                               " ".join(context.user_data["remaining_chars"]),
                               parse_mode=ParseMode.MARKDOWN_V2)  # , reply_markup=reply_markup)
     return "guess"
@@ -128,24 +160,30 @@ def main() -> None:
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    dispatcher.add_handler(CallbackQueryHandler(show_back_home, pattern="return_menu"))
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler('start', start),
+                      CallbackQueryHandler(start, pattern="start_game")],
         states={
             "start": [
-                CallbackQueryHandler(start_game, pattern="Yes"),
+                CallbackQueryHandler(start_game, pattern="start_game"),
             ],
             "guess": [
                 MessageHandler(
                     Filters.all, verify_guess
-                )
+                ),
             ],
             "win": [
 
+            ],
+            "lose": [
+                CallbackQueryHandler(start_game, pattern="start_game"),
+                CallbackQueryHandler(start, pattern="return_menu")
             ]
 
         },
         fallbacks=[MessageHandler(Filters.regex('^Done$'), start)],
+
     )
 
     dispatcher.add_handler(conv_handler)
